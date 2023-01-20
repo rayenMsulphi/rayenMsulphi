a81875c1b3c0cf24e2e7d8251adb8095c08c4364
rayenMsulphi <63091135+rayenMsulphi@users.noreply.github.com>
2023-01-20 20:05:29 +0530

    Add files via upload

---
+++ anim.js
@@ -0,0 +1,11 @@
+function f1() {
+    for(let x=1;x<16;x++){
+        document.getElementById("seb").style.top = Math.random()*300 + "px";
+        document.getElementById("seb").style.left = Math.random()*300 + "px";
+    }
+}
+
+function showAlert(){
+    const sAlert="thenkuuu!!";
+    alert(sAlert);
+}
